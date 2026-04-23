31132c4f04a39bc3605777bf7f45031b53f36487
Sampaniony Rakotoarivelo <sampanionyra55@gmail.com>
2026-04-23 16:18:15 +0200

    feat: move font import to layout component for better performance

--- apps/web-app/src/app/layout.tsx
+++ apps/web-app/src/app/layout.tsx
@@ -14,6 +14,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
     return (
         <html lang="fr">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
             <body>
                 <AuthProvider accessToken={accessToken}>
                     {children}
